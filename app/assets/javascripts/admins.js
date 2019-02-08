84d9e8c566403fc9f6b0df1264e8455c201e54c2
Laurence Bautista <rorens05@gmail.com>
2019-02-08 13:07:18 +0800

    added little responsiveness

---
+++ app/assets/javascripts/admins.js
@@ -0,0 +1,25 @@
+
+function resize() {
+  if ($(window).width() < 514) {
+    $('.sidebar').addClass('hidden');
+    $('.content').addClass('fullscreen');
+  }
+  else {
+    $('sidebar').removeClass('hidden');
+  }
+}
+
+
+$( document ).ready(function() {
+  $(window).resize(resize);
+  resize();
+  
+  $(".toggle").click(function(){
+    $(".sidebar").toggleClass("hidden");
+    $(".content").toggleClass("fullscreen");
+    $(".admin-name").toggleClass("hidden");;
+    $(".controller-name").toggleClass("hidden");
+    $(".body").toggleClass("hidden");
+    console.log("toggled");
+  });
+});
