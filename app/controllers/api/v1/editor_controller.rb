a4ee902960a8c72c1d2ced2fc44415e332848dd4
Laurence Bautista <rorens05@gmail.com>
2019-02-27 04:28:55 +0800

    finish android initial release

--- app/controllers/api/v1/editor_controller.rb
+++ app/controllers/api/v1/editor_controller.rb
@@ -76,7 +76,53 @@ module Api
       end
 
       def new_game
+        name = params[:name]
+        event_id = params[:event_id]
+        game_type = params[:game_type]
+        sport_id = params[:sport_id]
+        schedule = params[:schedule]
+        status = params[:status]
+        created_by_id = params[:created_by_id]
+        updated_by_id = params[:updated_by_id]
+        team2_id = params[:team2_id]
+        team1_id = params[:team1_id]
         
+        game = Game.new
+        game.name = name
+        game.event_id = event_id
+        game.game_type = game_type
+        game.sport_id = sport_id
+        game.schedule = DateTime.parse(schedule)
+        game.status = status
+        game.created_by_id = created_by_id
+        game.last_updated_by_id = updated_by_id
+        
+        if game.valid?
+          game.save
+
+          team1 = ContestantTeam.new
+          team1.team_id = team1_id
+          team1.score = 0
+          team1.game_id = game.id
+
+          team2 = ContestantTeam.new
+          team2.team_id = team2_id
+          team2.score = 0   
+          team2.game_id = game.id
+
+          if team1.valid? && team2.valid?
+            team1.save
+            team2.save
+            
+            render json: {status: 'success', message: "game saved"}, status: :ok
+          else            
+            render json: {status: 'failed', message: team1.errors.full_messages}, status: :ok
+          end
+
+        else
+          render json: {status: 'failed', message: game.errors.full_messages}, status: :ok
+
+        end
       end
 
       def edit_game
